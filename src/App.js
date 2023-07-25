00a2be211d5c21e5de7e4eb933cbf0d76d17d922
Ross <rosskeene196@gmail.com>
2023-07-24 21:20:47 -0500

    saved

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import React, { useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API endpoint
+    fetch('http://localhost:3000/bigqueryData')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+  console.log(data);
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Zip</th>
+          <th>Hole Count</th>
+          <th>Rating</th>
+          <th>Latitude</th>
+          <th>Longitude</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.city}</td>
+            <td>{item.state}</td>
+            <td>{item.zip}</td>
+            <td>{item.holeCount}</td>
+            <td>{item.rating}</td>
+            <td>{item.latitude}</td>
+            <td>{item.longitude}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+
+export default App;
